90cd8f98f478ff070191e2944d37eecb5e88637e
Brunei <85696954+2649426633@users.noreply.github.com>
2026-08-08 10:46:07 +0800

    净化在线检测设计时构造路径

--- src/IAD/Pages/OnlineInspectionPage.cs
+++ src/IAD/Pages/OnlineInspectionPage.cs
@@ -1,18 +1,22 @@
-using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace IAD.Pages
 {
     public partial class OnlineInspectionPage : UserControl
     {
+        private bool runtimeInitialized;
+
         public OnlineInspectionPage()
         {
             InitializeComponent();
-            if (LicenseManager.UsageMode != LicenseUsageMode.Designtime)
-            {
-                LoadSampleData();
-                BindEvents();
-            }
+        }
+
+        public void InitializeRuntime()
+        {
+            if (runtimeInitialized) return;
+            runtimeInitialized = true;
+            LoadSampleData();
+            BindEvents();
         }
 
         private void BindEvents()
