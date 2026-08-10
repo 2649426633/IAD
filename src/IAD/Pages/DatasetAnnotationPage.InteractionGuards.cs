ca7605dd8b45e07ac626c5ad8941147f63fa7118
Brunei <85696954+2649426633@users.noreply.github.com>
2026-08-10 10:34:29 +0800

    接入Mask编辑器运行时初始化

--- src/IAD/Pages/DatasetAnnotationPage.InteractionGuards.cs
+++ src/IAD/Pages/DatasetAnnotationPage.InteractionGuards.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace IAD.Pages
 {
     public partial class DatasetAnnotationPage
     {
-        private bool correctingEditButtonColor;
-
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -17,8 +14,7 @@ namespace IAD.Pages
             dgvClasses.Enter += NonCanvasControl_Enter;
             dgvLayers.Enter += NonCanvasControl_Enter;
             cboCurrentClass.Enter += NonCanvasControl_Enter;
-            btnMaskEdit.BackColorChanged += btnMaskEdit_BackColorChanged;
-            EnsureEditButtonHighlight();
+            InitializeMaskEditorRuntime();
         }
 
         private void NonCanvasControl_Enter(object sender, EventArgs e)
@@ -26,30 +22,6 @@ namespace IAD.Pages
             selectedAnnotationId = 0;
             CancelAnnotationEdit();
             if (pnlCanvas != null) pnlCanvas.Invalidate();
-        }
-
-        private void btnMaskEdit_BackColorChanged(object sender, EventArgs e)
-        {
-            EnsureEditButtonHighlight();
-        }
-
-        private void EnsureEditButtonHighlight()
-        {
-            if (correctingEditButtonColor || btnMaskEdit == null) return;
-            Color desired = string.Equals(activeTool, "Select", StringComparison.Ordinal)
-                ? Color.FromArgb(210, 225, 242)
-                : UiTheme.Surface;
-            if (btnMaskEdit.BackColor == desired) return;
-
-            correctingEditButtonColor = true;
-            try
-            {
-                btnMaskEdit.BackColor = desired;
-            }
-            finally
-            {
-                correctingEditButtonColor = false;
-            }
         }
     }
 }
