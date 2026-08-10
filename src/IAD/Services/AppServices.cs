83af590567d37c002d329ed5c584cbeaf16f1d91
Brunei <85696954+2649426633@users.noreply.github.com>
2026-08-10 10:31:27 +0800

    注册Mask服务并应用V7扩展

--- src/IAD/Services/AppServices.cs
+++ src/IAD/Services/AppServices.cs
@@ -12,6 +12,7 @@ namespace IAD.Services
         public static ProductService Products { get; private set; }
         public static DatasetService Datasets { get; private set; }
         public static AnnotationEditingService AnnotationEditing { get; private set; }
+        public static DatasetMaskService Masks { get; private set; }
         public static DefectRecognitionService DefectRecognition { get; private set; }
         public static RecipeService Recipes { get; private set; }
         public static ResultService Results { get; private set; }
@@ -25,6 +26,7 @@ namespace IAD.Services
 
             SqliteConnectionFactory connectionFactory = new SqliteConnectionFactory(ProjectStoragePaths.DatabasePath);
             DatabaseInitializer.Initialize(connectionFactory);
+            DatasetMaskDatabaseMigration.Apply(connectionFactory);
 
             IProductRepository productRepository = new ProductRepository(connectionFactory);
             IProductDefinitionSettingsRepository definitionSettingsRepository = new ProductDefinitionSettingsRepository(connectionFactory);
@@ -33,14 +35,19 @@ namespace IAD.Services
             IInspectionRecipeRepository recipeRepository = new InspectionRecipeRepository(connectionFactory);
             IInspectionResultRepository resultRepository = new InspectionResultRepository(connectionFactory);
             IDatasetRepository datasetRepository = new DatasetRepository(connectionFactory);
+            IDatasetMaskRepository maskRepository = new DatasetMaskRepository(connectionFactory);
             IDefectRecognitionRepository recognitionRepository = new DefectRecognitionRepository(connectionFactory);
 
             Products = new ProductService(productRepository, definitionSettingsRepository, categoryRepository, roiRepository);
             Datasets = new DatasetService(productRepository, definitionSettingsRepository, categoryRepository, datasetRepository);
             AnnotationEditing = new AnnotationEditingService(datasetRepository, categoryRepository);
+            Masks = new DatasetMaskService(datasetRepository, categoryRepository, maskRepository);
             DefectRecognition = new DefectRecognitionService(productRepository, categoryRepository, Datasets, recognitionRepository);
             Recipes = new RecipeService(productRepository, recipeRepository);
             Results = new ResultService(productRepository, resultRepository);
+
+            try { Masks.CleanupOrphanFiles(); }
+            catch { }
 
             initialized = true;
         }
